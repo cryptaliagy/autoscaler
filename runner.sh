e4140c7e88509881fb3625c93d01febedff1df6d
Natalia Maximo <natalia.maximo@uottawa.ca>
2021-12-17 13:38:12 -0500

    chg: update PATH as part of the runner script

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export PATH="/home/runner/.local/bin":$PATH
 echo "Connecting to the Github runner"
 ./config.sh --unattended --url https://github.com/$USER/$REPO --token $TOKEN --ephemeral
 ./run.sh
